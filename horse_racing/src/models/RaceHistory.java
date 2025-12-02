5ac1f0c30aebdc044a0c451b19d9bffbc622375d
Izzatongithub <izzatnazhiefa24@gmail.com>
2025-12-03 00:32:46 +0800

    add models

---
+++ horse_racing/src/models/RaceHistory.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class RaceHistory extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+    
+    private int userId;
+    private String horseName;
+    private int position;
+    private int totalHorses;
+    private int coinsEarned;
+    private String timestamp;
+    
+    public RaceHistory(int userId, String horseName, int position, int totalHorses, int coinsEarned) {
+        this.userId = userId;
+        this.horseName = horseName;
+        this.position = position;
+        this.totalHorses = totalHorses;
+        this.coinsEarned = coinsEarned;
+        this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    }
+    
+    @Override
+    public String getName() {
+        return horseName;
+    }
+    
+    @Override
+    public String getInfo() {
+        return toString();
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+    
+    public String getHorseName() {
+        return horseName;
+    }
+    
+    public int getPosition() {
+        return position;
+    }
+    
+    public int getTotalHorses() {
+        return totalHorses;
+    }
+    
+    public int getCoinsEarned() {
+        return coinsEarned;
+    }
+    
+    public String getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(Timestamp ts) {
+        this.timestamp = ts.toLocalDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[%s] Position %d/%d - Earned: %d coins", 
+                           timestamp, position, totalHorses, coinsEarned);
+    }
+}
---
+++ horse_racing/src/models/User.java
@@ -0,0 +1,73 @@
+package models;
+
+public class User extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+    
+    private int userId;
+    private String username;
+    private String password;
+    private Horse horse;
+    private int coins;
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.coins = 500;
+        this.horse = null;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    @Override
+    public String getName() {
+        return username;
+    }
+    
+    @Override
+    public String getInfo() {
+        return String.format("User: %s - Coins: %d, Horse: %s", 
+                           username, coins, horse != null ? horse.getName() : "None");
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public Horse getHorse() {
+        return horse;
+    }
+    
+    public void setHorse(Horse horse) {
+        this.horse = horse;
+    }
+    
+    public int getCoins() {
+        return coins;
+    }
+    
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+    
+    public void addCoins(int amount) {
+        this.coins += amount;
+    }
+    
+    public boolean spendCoins(int amount) {
+        if (coins >= amount) {
+            coins -= amount;
+            return true;
+        }
+        return false;
+    }
+}
